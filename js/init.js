7e4c366ed894c8eed32de218717e47bbaf21502a
natalysilva99 <88351497+natalysilva99@users.noreply.github.com>
2021-10-11 18:42:45 -0300

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,110 @@
+const CATEGORIES_URL = "https://japdevdep.github.io/ecommerce-api/category/all.json";
+const PUBLISH_PRODUCT_URL = "https://japdevdep.github.io/ecommerce-api/product/publish.json";
+const CATEGORY_INFO_URL = "https://japdevdep.github.io/ecommerce-api/category/1234.json";
+const PRODUCTS_URL = "https://japdevdep.github.io/ecommerce-api/product/all.json";
+const PRODUCT_INFO_URL = "https://japdevdep.github.io/ecommerce-api/product/5678.json";
+const PRODUCT_INFO_COMMENTS_URL = "https://japdevdep.github.io/ecommerce-api/product/5678-comments.json";
+const CART_INFO_URL = "https://japdevdep.github.io/ecommerce-api/cart/987.json";
+const CART_BUY_URL = "https://japdevdep.github.io/ecommerce-api/cart/buy.json";
+
+var showSpinner = function(){
+  document.getElementById("spinner-wrapper").style.display = "block";
+}
+
+var hideSpinner = function(){
+  document.getElementById("spinner-wrapper").style.display = "none";
+}
+
+var getJSONData = function(url){
+    var result = {};
+    showSpinner();
+    return fetch(url)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }else{
+        throw Error(response.statusText);
+      }
+    })
+    .then(function(response) {
+          result.status = 'ok';
+          result.data = response;
+          hideSpinner();
+          return result;
+    })
+    .catch(function(error) {
+        result.status = 'error';
+        result.data = error;
+        hideSpinner();
+        return result;
+    });
+}
+
+//creo una variable global 
+//id = donde aparecera el nombre del usuario 
+var usuarionav = document.getElementById("saludousuario");
+
+//creo una funcion 
+function mostrar(){
+
+//creo una variable local 
+//extraigo el JSON con getItem 
+let username = JSON.parse(localStorage.getItem("user"));
+
+if (username == undefined){
+  location.href = "index.html"
+}
+//getItem es la sintaxis para leer el ítem almacenado en localStorage
+else{
+  let {names, emails} = JSON.parse(localStorage.getItem("user"));
+  usuarionav.innerHTML+=
+   names;
+
+};
+};
+
+document.addEventListener("DOMContentLoaded", function(e){
+mostrar();
+});
+
+
+//funcion para borrar los datos del usuraio registrado una vez que accede a cerrar sesión
+ 
+function logout(){
+  localStorage.clear();
+  window.location.replace('index.html');
+};
+
+  
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 4000);    
+}
+
+// Used to toggle the menu on small screens when clicking on the menu button
+function myFunction() {
+  var x = document.getElementById("navDemo");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+
+// When the user clicks anywhere outside of the modal, close it
+var modal = document.getElementById('ticketModal');
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
